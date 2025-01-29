2c76a28ff11193f001fdf0a2be452894dfea2eaf
Torubiri Walter <toruwalt1997@gmail.com>
2025-01-29 17:59:36 +0100

    Jsonified HTTP response of email, current time and github URL

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """HNG Backend Stage 0"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +11,13 @@ CORS(app)
 
 @app.route("/", methods=['GET'])
 def display_info():
-    return "Hi"
+    info = {
+            "email": "toruwalt1997@gmail.com",
+            "current_datetime": datetime.now().isoformat(),
+            "github_url": "https://github.com/toruwalt/hng12-backend-stage0"
+            }
+
+    return jsonify(info)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
